13e0e2da072bef688ed5d730b0d26c0b77794715
BudaViktor <vicktor0095@gmail.com>
2021-11-02 21:59:50 +0100

    névkereső modosult

--- Python/nevkereso_class.py
+++ Python/nevkereso_class.py
@@ -2,47 +2,47 @@ import fajlbeolvasas
 
 class Search_name:
 	
-	def __init__ (self, lista):
-		
+    def __init__ (self, lista):
+    
         self.lista = lista
-		if 0 == len(self.lista):
-			self.txt = "\tNincs betöltött fájl! Kérlek válassz egy fájlt a Főmenüben."
-			print(self.txt)
-			self.txt = "\tKérlek nyomj ENTER-t  a vissza lépéshez!"
-			input(self.txt)
-			return 
+        if 0 == len(self.lista):
+            self.txt = "\tNincs betöltött fájl! Kérlek válassz egy fájlt a Főmenüben."
+            print(self.txt)
+            self.txt = "\tKérlek nyomj ENTER-t  a vissza lépéshez!"
+            input(self.txt)
+            return 
+           
+        self.txt = "\n\tÜdvözöllek a Névkeresőben!\n"
+        print(self.txt)
 		
-		self.txt = "\n\tÜdvözöllek a Névkeresőben!\n"
-		print(self.txt)
+        self.ciklus_n = True
+        self.i = 0
+        self.b = 0
 		
-		self.ciklus_n = True
-		self.i = 0
-		self.b = 0
+        Search_name.ciklus(self)
 		
-		Search_name.ciklus(self)
+    def question(self):
+        print("OK")
+        return Search_name.ciklus(self)
 		
-	def question(self):
-		print("OK")
-		return Search_name.ciklus(self)
-		
-	def ciklus(self, lista):
+    def ciklus(self, lista):
 	
-		self.txt = "\t A keresett festmény: "
-		self.name = str(input(self.txt))
+        self.txt = "\t A keresett festmény: "
+        self.name = str(input(self.txt))
 		
-		while self.ciklus_n == True:
-			self.i += 1
+        while self.ciklus_n == True:
+            self.i += 1
 			
-			for x in range (len(self.lista)):
-				if self.name == self.lista[x]:
-					self.txt = "\t   <lista ára és stílusa> \n"
-					print(self.txt)
-					return Search_name.question(self)
-				else:
-					self.b += 1
+            for x in range (len(self.lista)):
+                if self.name == self.lista[x]:
+                    self.txt = "\t   <lista ára és stílusa> \n"
+                    print(self.txt)
+                    return Search_name.question(self)
+                else:
+                    self.b += 1
 
-			if self.b > 0:
-				self.txt = "nincs ilyen a listában!"
-				print(self.txt)
-				return Search_name.question(self)
+            if self.b > 0:
+                self.txt = "nincs ilyen a listában!"
+                print(self.txt)
+                return Search_name.question(self)
 
