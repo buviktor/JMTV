315ac74300cdc3835b79a8daf8df5a856d6b5345
BenkeM <benke.mariaa@gmail.com>
2021-11-02 20:57:02 +0100

    fájlkiírás -tab

--- Python/fajl_kiiras.py
+++ Python/fajl_kiiras.py
@@ -3,12 +3,12 @@ import festmenyek_class
 class Kiiras:
     def __init__(self, lista):
    
-        print("\tA fájl adatai a következők: ")
+        print("A fájl adatai a következők: ")
 
         for i in range(len(lista)):
               print(lista[i].toString())
 
-        print("\tA főmenübe való visszalépéshez kérem nyomja meg az entert")
+        print("A főmenübe való visszalépéshez kérem nyomja meg az entert")
         input("<<Enter>>")
         return
 
