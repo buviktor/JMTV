0c27fc3becc4facf4ac288b7c74516a261139243
tundeeva <92318536+tundeeva@users.noreply.github.com>
2021-11-03 21:56:34 +0100

    Add files via upload

--- Python/statisztika_class.py
+++ Python/statisztika_class.py
@@ -4,18 +4,24 @@ class Statisztika:
     def __init__(self, lista):
         self.lista = lista
         ertekek = []
+
         print("Statisztika a feltöltött festményekről:\n")
         for i in range(len(lista)):
             ertekek.append(lista[i].ertek)
+
         print("\nA legdrágább festmény(ek):")
-        for i in range(len(lista)):
-            if max(ertekek) == lista[i].ertek:
-                print("\t" + lista[i].toString)
+        for i in range(len(self.lista)):
+            if max(ertekek) == self.lista[i].ertek:
+                print(self.lista[i].toString())
+
         print("\nA legolcsóbb festmény(ek):")
-        for i in range(len(lista)):
-            if min(ertekek) == lista[i].ertek:
-                print("\t" + lista[i].toString)
-        print("\nA festmények átlagértéke:", str(sum(ertekek)/len-(ertekek))
+        for i in range(len(self.lista)):
+            if min(ertekek) == self.lista[i].ertek:
+                print(self.lista[i].toString())
+
+        print("\nA festmények átlagértéke:", str(sum(ertekek)/len(ertekek)), "Ft"
+        print("A főmenübe való visszalépéshez kérem nyomja meg az enter")
+        input("<<Enter>>")
         return
 
 
