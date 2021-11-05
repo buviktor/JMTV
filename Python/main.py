0a4703712e89c9d88f8b68497ce4d2252f5c2e63
janos1109 <janos1109@gmail.com>
2021-11-05 20:11:30 +0100

    Frissítve Viktor változtatásai szerint

--- Python/main.py
+++ Python/main.py
@@ -16,51 +16,54 @@ def main():
     answer = ""
     volt_betoltes = False
     hiba_uzenet = 30*"-" + "\n**Nincs feltöltött fájl még!**\n" + 30*"-"
-
-    while 1:
+    
+    while answer != 'exit':
         menu_kiiro.Fomenu()
         prompt = ">"
-        while answer != 'exit':
-            answer = input(prompt)
-            if answer == 'a':
-                fajlbeolvasas.Beolvasas(rekordok)
-                volt_betoltes = True
-                break
-            elif answer == 'b':
-                if volt_betoltes == True:
-                    fajl_kiiras.Kiiras(rekordok)
-                    break
-                else:
-                    print(hiba_uzenet)
-            elif answer == 'c':
-                if volt_betoltes == True:
-                    festmeny_hozzaadas.FestményHozzáadás()
-                    break
-                else:
-                    print(hiba_uzenet)
-            elif answer == 'd':
-                if volt_betoltes == True:
-                    nevkereso_class.Search_name(rekordok)
-                    break
-                else:
-                    print(hiba_uzenet)
-            elif answer == 'e':
-                if volt_betoltes == True:
-                    stiluskereso_class.Search_style(rekordok)
-                    break
-                else:
-                    print(hiba_uzenet)
-            elif answer == 'f':
-                if volt_betoltes == True:
-                    statisztika_class.Statisztika(rekordok)
-                    break
-                else:
-                    print(hiba_uzenet)
-            elif answer == 'exit':
-                quit()
-            answer = ""
+        
+        answer = input(prompt)
+        if answer == 'a':
+            fajlbeolvasas.Beolvasas(rekordok)
+            volt_betoltes = True
+            continue
+            
+        elif answer == 'b':
+            if volt_betoltes == True:
+                 fajl_kiiras.Kiiras(rekordok)
+                 continue
+            else:
+                print(hiba_uzenet)
+                
+        elif answer == 'c':
+            if volt_betoltes == True:
+                festmeny_hozzaadas.FestményHozzáadás()
+                continue
+            else:
+                print(hiba_uzenet)
+                
+        elif answer == 'd':
+            if volt_betoltes == True:
+                nevkereso_class.Search_name(rekordok)
+                continue
+            else:
+                print(hiba_uzenet)
+                
+        elif answer == 'e':
+            if volt_betoltes == True:
+                stiluskereso_class.Search_style(rekordok)
+                continue
+            else:
+                print(hiba_uzenet)
+                
+        elif answer == 'f':
+            if volt_betoltes == True:
+                statisztika_class.Statisztika(rekordok)
+                continue
+            else:
+                print(hiba_uzenet)
+                
+        elif answer == 'exit':
+            quit()
     
-
-
 
 main()
